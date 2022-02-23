5f9956c801ca6e2c6135ebcff8b44852fdcd441c
noctwolf <noctwolf@gmail.com>
2022-02-23 14:37:18 +0800

    添加项目文件。

---
+++ PrismSkin.Themes/ThemesModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismSkin.Themes.Views;
+
+namespace PrismSkin.Themes
+{
+    public class ThemesModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ PrismSkin/Views/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrismSkin.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button)) return;
+            Application.Current.Resources.MergedDictionaries.Clear();
+            AppDomain.CurrentDomain.GetAssemblies().Where(f => !f.GlobalAssemblyCache && !f.IsDynamic).ToList().ForEach(f =>
+            {
+                try
+                {
+                    Uri uri = new Uri($"/{f.GetName()};component/Themes/Themes{button.Content}/Dictionary.xaml", UriKind.Relative);
+                    _ = Application.GetResourceStream(uri);//如果资源不存在，跳过下面的行
+                    Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = uri });
+                }
+                catch (IOException ex) when (new StackTrace(ex).GetFrames().Reverse().Skip(1).First().GetMethod().Name == nameof(Application.GetResourceStream))
+                {
+                    // ignored
+                }
+            });
+        }
+    }
+}
